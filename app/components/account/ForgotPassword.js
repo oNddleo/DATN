e86eb685f904ba30deca0fb38ff4397c2941f34d
Nguyen Duc Long <nguyenduclong12a2nd@gmail.com>
2018-06-13 17:23:42 +0700

    add screen forgot password

---
+++ app/components/account/ForgotPassword.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class ForgotPassword extends Component {
+    render() {
+        return (
+            <div className="forgot-password">
+                <div className="container">
+                    <div className="mod-layout">
+                        <div className="mod-layout-title">
+                            <h2>Quên mật khẩu?</h2>
+                        </div>
+                        <div className="mod-layout-main">
+                            <div className="mod-layout-subtitle">
+                                <h3>Nhập địa chỉ email của bạn dưới đây và chúng tôi sẽ gửi cho bạn một liên kết để đặt lại mật khẩu của bạn.</h3>
+                            </div>
+                            <div class="forgot">
+                                <div class="forgot-form">
+                                    <div class="mod-input mod-input-email">
+                                        <label>Địa chỉ email</label>
+                                        <input type="text" placeholder="Vui lòng nhập email của bạn" value="" />
+                                    </div>
+                                </div>
+                                <div className="space20"></div>
+                                <div class="forgot-action">
+                                    <button type="button" class="forgot-password-btn">GỬI</button>
+                                </div>
+                                <div className="space10"></div>
+                                <div class="forgot-ft">
+                                    <a href="#">Quay lại</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ForgotPassword;
