fb966dc6329240d5b3894ee1866011fc8e510124
Nguyen Duc Long <nguyenduclong12a2nd@gmail.com>
2018-06-15 23:40:49 +0700

    update response userscontroller

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -4,37 +4,39 @@
  * @description :: Server-side logic for managing users
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var Encrypt = require('../utils/Encrypt')
+var Message = require('../utils/Message')
+var Type = require('../utils/Type')
 module.exports = {
-
-    create: (req, res) => {
+    create: async (req, res) => {
         let data = req.body;
-        Users.create(data).exec((err, result) => {
+        let passEncrypt = await Encrypt.encryptPassword(req.body.password);
+        data.password = passEncrypt;
+        Users.create({...data}).exec((err, result) => {
             if (err) {
-                sails.log("error========", err)
-                res.send({ err: err, message: "Không thêm mới được người dùng" })
+                return res.send(Message.sendErrorJSON(err, Type.errorMessage.USER_REGISTED))
             }
             //Should response message send client ,not send data
-            return res.send(result);
+            return res.redirect('/');
         })
     },
-    update: (req, res) => {
+    update: async (req, res) => {
         let data  = req.body;
+        let passEncrypt = await Encrypt.encryptPassword(req.body.password);
+        data.password = passEncrypt;
         Users.update({ where: { id: req.params.id } }, data ).exec((err, updated) => {
             if (err) {
-                sails.log("error========", err)
-                res.send({ err: err, message: "Không tìm thấy người dùng" })
+                res.send(Message.sendErrorJSON(err, Type.errorMessage.USER_NOT_FOUND))
             }
-            return res.send(updated);
+            return res.send(Message.sendSuccessJSON(Type.successMessage.USER_UPDATED, updated))
         })
     },
     delete: (req, res) => {
         Users.destroy({ where: { id: req.params.id } }).exec((err, result) => {
             if (err) {
-                sails.log("error========", err)
-                res.send({ err: err, message: "Không tìm thấy người dùng" })
+                res.send(Message.sendErrorJSON(err, Type.errorMessage.USER_NOT_FOUND))
             }
-            return res.send(result)
+            return res.send(Message.sendSuccessJSON(Type.successMessage.USER_DELETED, result))
         })
     }
 };
