45fe1997fe96d9abf4a4727ffb7895c4cca48fd4
Nguyen Duc Long <nguyenduclong12a2nd@gmail.com>
2018-06-15 23:40:18 +0700

    create message

---
+++ api/utils/Type.js
@@ -0,0 +1,18 @@
+module.exports = {
+    successMessage:{
+        LOGIN_SUCCESS: "Đăng nhập thành công",
+        USER_UPDATED: "Update thông tin thành công",
+        USER_CREATE: "Thêm người dùng thành công",
+        USER_DELETED: "Xoá người dùng thành công"
+    },
+    errorMessage: {
+        USER_NOT_FOUND: "Không thêm mới được người dùng",
+        USER_NOT_EXIST: "Người dùng không tồn tại",
+        USER_REGISTED: "Email đã được sử dụng",
+        USER_NOT_TRUE: "Email không đúng định dạng",
+        
+        PASS_NOT_MATCH: "Mật khẩu không khớp",
+        PASS_NOT_TRUE: "Mật khấu không đúng định dạng",
+        ERROR_SYSTEM: "Lỗi hệ thống"
+    },
+}
