26289437e5b09f9eb73168fe29a0f006f7fc58d7
Nguyen Duc Long <nguyenduclong12a2nd@gmail.com>
2018-06-20 08:05:44 +0700

    update image controller

---
+++ api/controllers/ImageController.js
@@ -0,0 +1,62 @@
+var Message = require('../utils/Message')
+var Type = require('../utils/Type')
+module.exports = {
+  upload: function (req, res) {
+    let file = req.file('file');
+    file.upload({
+      maxBytes: 100000000,
+      // set the directory
+      dirname: '../../assets/images/testUpload'
+    }, function (err, upload) {
+      console.log('upload', upload)
+      // try to always handle errors
+      if (err) { return res.serverError(err) }
+      // upload is an array of files uploaded 
+      // so remember to expect an array object
+      if (upload.length === 0) { return res.badRequest('No file was uploaded') }
+      // if file was uploaded create a new registry
+      // at this point the file is phisicaly available in the hard drive
+      Image.create({
+        path: upload[0].fd,
+        fileName: upload[0].filename,
+      }).exec(function (err, result) {
+        if (err) { return res.serverError(err) }
+        // if it was successful return the registry in the response
+        return res.json(result.id)
+      })
+    })
+  },
+  download: function (req, res) {
+    var fileID = req.param('id')
+    // gets the id either in urlencode, body or url query
+    File
+      .findOne({ id: fileID })
+      .exec((err, file) => {
+        if (err) { return res.serverError(err) }
+        if (!file) { return res.notFound() }
+        // we use the res.download function to download the file 
+        // and send a ok response
+        res.download(file.path, function (err) {
+          if (err) {
+            return res.serverError(err)
+          } else {
+            return res.ok()
+          }
+        })
+      })
+  },
+  delete: function (req, res) {
+    Image.destroy({ where: { id: req.params.id } }).exec((err, result) => {
+      if (err) {
+          res.send(Message.sendErrorJSON(err, Type.errorMessage.IMAGE_NOT_FOUND))
+      }
+      return res.send(Message.sendSuccessJSON(Type.successMessage.IMAGE_DELETED, result))
+  })
+  },
+  get: function(req, res){
+    Image.find().populate("productID", {select: ['productID']}).exec(function(err, result){
+      if(err) res.send(Message.sendErrorJSON(err, Type.errorMessage.IMAGE_NOT_FOUND))
+      return res.send(result)
+    })
+  }
+}
