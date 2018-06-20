96bcf7ad13a264c4d6da429935ebe0279f98a4f8
Nguyen Duc Long <nguyenduclong12a2nd@gmail.com>
2018-06-20 08:07:22 +0700

    create new message

--- api/utils/Type.js
+++ api/utils/Type.js
@@ -1,18 +1,50 @@
 module.exports = {
     successMessage:{
+        /**
+         * Message user
+         */
         LOGIN_SUCCESS: "Đăng nhập thành công",
-        USER_UPDATED: "Update thông tin thành công",
+        USER_UPDATED: "Update thông tin người dùng thành công",
         USER_CREATE: "Thêm người dùng thành công",
-        USER_DELETED: "Xoá người dùng thành công"
+        USER_DELETED: "Xoá người dùng thành công",
+
+        /**
+         * Message product
+         */
+        PRODUCT_CREATED: "Thêm sản phẩm thành công",
+        PRODUCT_DELETED: "Xoá sản phẩm thành công",
+        PRODUCT_UPDATED: "Update thông tin sản phẩm thành công",
+        PRODUCT_INFO: "Lấy dữ liệu thành công",
+        /**
+         * Message image
+         */
+        IMAGE_DELETED: "Xoá ảnh thành công"
     },
     errorMessage: {
+        /**
+         * Message user
+         */
         USER_NOT_FOUND: "Không thêm mới được người dùng",
         USER_NOT_EXIST: "Người dùng không tồn tại",
         USER_REGISTED: "Email đã được sử dụng",
         USER_NOT_TRUE: "Email không đúng định dạng",
-        
+        /**
+         * Message password
+         */
         PASS_NOT_MATCH: "Mật khẩu không khớp",
         PASS_NOT_TRUE: "Mật khấu không đúng định dạng",
-        ERROR_SYSTEM: "Lỗi hệ thống"
+        ERROR_SYSTEM: "Lỗi hệ thống",
+
+        /**
+         * Message product
+         */
+        PRODUCT_NOT_CREATED: "Thêm sản phẩm không thành công",
+        PRODUCT_NOT_DELETED: "Xoá sản phẩm không thành công",
+        PRODUCT_NOT_FOUND: "Không tìm thấy sản phẩm",
+        /**
+         * Message image
+         */
+        IMAGE_NOT_DELETED: "Xoá ảnh thành công",
+        IMAGE_NOT_UPDATE: "Update ảnh không thành công"
     },
 }
